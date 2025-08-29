12594817bfef828f274b9f5f14cec2dcdc175c27
SumantaSwainEpam <sumanta_swain@epam.com>
2025-08-29 19:48:03 +0530

    ok done

---
+++ Framework/Logs/LogManaging.cs
@@ -0,0 +1,44 @@
+﻿using log4net.Config;
+using log4net;
+using System.Reflection;
+using System.IO;
+
+namespace SwagLabTestBDDFramework.Framework.Logs
+{
+     public  class LogManaging
+     {
+        private static readonly ILog _log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void InitializeLogging()
+        {
+            
+
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var log4NetConfig = Path.Combine(baseDir, "Framework", "Credentials", "Log4net.config");
+
+            
+            var logFilePath = Path.Combine(baseDir, "Framework", "Logs");
+            if (!Directory.Exists(logFilePath))
+            {
+                Directory.CreateDirectory(logFilePath);
+            }
+
+            if (File.Exists(log4NetConfig))
+            {
+                XmlConfigurator.ConfigureAndWatch(new FileInfo(log4NetConfig));
+            }
+
+            _log.Info("Logging has been initialized.");
+        
+        }
+
+        
+        public static ILog GetLogger()
+        {
+            var callingType = MethodBase.GetCurrentMethod().DeclaringType;
+            return log4net.LogManager.GetLogger(callingType);
+        }
+    }
+}
+
+
